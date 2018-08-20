b56ff9c3962105d9b6eb29f7d066be9cb4321b65
drakonli <artur.drakonli@gmail.com>
2018-08-21 01:06:53 +0300

    - removed unused saveHandler

--- src/main/java/drakonli/dota2/hero_grid_customizer/application/view_handler/save/SaveButtonHandlerInterface.java
+++
@@ -1,7 +0,0 @@
-package drakonli.dota2.hero_grid_customizer.application.view_handler.save;
-
-import drakonli.dota2.hero_grid_customizer.application.view_handler.ViewActionHandlerInterface;
-
-public interface SaveButtonHandlerInterface extends ViewActionHandlerInterface
-{
-}
